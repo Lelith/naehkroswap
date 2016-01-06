56194db875a03ff101945eb7e624a993acd44dd2
lelith <lelith@gmx.de>
2016-01-06 12:26:55 +0100

    add translations

---
+++ naehkroswap/event/main_listener.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ *
+ * This file is part of the phpBB Forum Software package.
+ *
+ * @copyright (c) phpBB Limited <https://www.phpbb.com>
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ * For full copyright and license information, please see
+ * the docs/CREDITS.txt file.
+ *
+ */
+
+namespace naehkromanten\naehkroswap\event;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Event listener
+ */
+class main_listener implements EventSubscriberInterface
+{
+    static public function getSubscribedEvents()
+    {
+        return array(
+            'core.user_setup' => 'load_language_on_setup',
+        );
+    }
+
+    public function load_language_on_setup($event)
+    {
+        $lang_set_ext = $event['lang_set_ext'];
+        $lang_set_ext[] = array(
+            'ext_name' => 'naehkromanten/naehkroswap',
+            'lang_set' => 'naehkroswap',
+        );
+        $event['lang_set_ext'] = $lang_set_ext;
+    }
+}
