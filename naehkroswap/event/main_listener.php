eb76bdd3a294c0f73b0c8327450605bff1ec6632
lelith <lelith@gmx.de>
2016-01-06 14:34:19 +0100

    set link in header

--- naehkroswap/event/main_listener.php
+++ naehkroswap/event/main_listener.php
@@ -24,7 +24,33 @@ class main_listener implements EventSubscriberInterface
   {
     return array(
         'core.user_setup' => 'load_language_on_setup',
+        'core.page_header' => 'add_page_header_link',
     );
+  }
+
+  /* @var \phpbb\controller\helper */
+  protected $helper;
+
+  /* @var \phpbb\template\template */
+  protected $template;
+
+  /**
+   * Constructor
+   *
+   * @param \phpbb\controller\helper $helper
+   * @param \phpbb\template\template $template
+   */
+  public function __construct(\phpbb\controller\helper $helper, \phpbb\template\template $template)
+  {
+      $this->helper = $helper;
+      $this->template = $template;
+  }
+
+  public function add_page_header_link($event)
+  {
+    $this->template->assign_vars(array(
+      'U_SWAP' => $this->helper->route('naehkromanten_naehkroswap_route', array('name' => 'world')),
+    ));
   }
 
   public function load_language_on_setup($event)
